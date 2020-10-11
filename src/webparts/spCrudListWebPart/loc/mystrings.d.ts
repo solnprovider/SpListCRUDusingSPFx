87e9831862a5e8b3060412d6c420f649b4ab72e7
solnprovider <enggvishal3@gmail.com>
2020-10-11 19:21:03 +0530

    CRUD on SharePoint list using SPFx
    Signed-off-by: solnprovider <enggvishal3@gmail.com>

---
+++ src/webparts/spCrudListWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpCrudListWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpCrudListWebPartWebPartStrings' {
+  const strings: ISpCrudListWebPartWebPartStrings;
+  export = strings;
+}
